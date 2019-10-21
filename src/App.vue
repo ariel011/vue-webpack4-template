a8912a68602f00b57be102182eea2210b534ccf7
ariel011 <a101496@aup.edu>
2019-10-21 14:20:37 +0000

    again

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,37 @@
 <template>
-  <div class="container center-content">
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col
-          cols="8"
-          offset="2"
-        >
-
-        <div>
-            <b-navbar toggleable="lg" type="dark" variant="info">
-                <b-navbar-brand href="#">My Restaurant</b-navbar-brand>
-
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-link to="/" router-tag="b-nav-item">Home</b-link>
-                    <b-link to="/About"  router-tag="b-nav-item">About</b-link>
-                    <b-link to="/Contact"  router-tag="b-nav-item">Contact</b-link>
-                    <b-link to="/Menu"  router-tag="b-nav-item">Menu</b-link>
-                </b-navbar-nav>
-
-                </b-collapse>
-            </b-navbar>
-            </div>
-
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12">
-          <RouterView />
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+ <div class="container center-content">
+   <b-container class="bv-example-row">
+     <b-row>
+       <b-col
+         cols="8"
+         offset="2"
+       >
+       <div>
+           <b-navbar toggleable="lg" type="dark" variant="info">
+               <b-navbar-brand href="#">My Restaurant</b-navbar-brand>
+               <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+               <b-collapse id="nav-collapse" is-nav>
+               <b-navbar-nav>
+                   <b-link to="/" router-tag="b-nav-item">Home</b-link>
+                   <b-link to="/about"  router-tag="b-nav-item">About</b-link>
+                   <b-link to="/menu" router-tag="b-nav-item">Menu</b-link>
+                    <b-link to="/contact" router-tag="b-nav-item">Contact</b-link>
+               </b-navbar-nav>
+               </b-collapse>
+           </b-navbar>
+           </div>
+       </b-col>
+     </b-row>
+     <b-row>
+       <b-col cols="12">
+         <RouterView />
+       </b-col>
+     </b-row>
+   </b-container>
+ </div>
 </template>
-
 <style lang="stylus" scoped>
-
 .container
-  font-family 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
-  max-width 1200px
-
+ font-family 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+ max-width 1200px
 </style>
